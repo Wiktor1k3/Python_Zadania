c3351d25c1c9aa1f9b771dace7c4af1d780c161f
Wiktor <wiktor1k32@onet.pl>
2021-03-11 13:53:40 +0100

    zadania nr2

---
+++ Zadania2/zad1.py
@@ -0,0 +1,7 @@
+lista = ["Piłka nożna", "Siatkówka", "Koszykówka", "Piłka ręczna"]
+print(lista)
+lista.reverse()
+print(lista)
+lista.append("Łyżwiarstwo")
+lista.append("Jeździectwo")
+print(lista)
