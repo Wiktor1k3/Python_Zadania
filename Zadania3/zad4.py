1cff97071494724f3842dd74a0e4b4c4087c6683
Wiktor <wiktor1k32@onet.pl>
2021-03-12 15:52:28 +0100

    Zadania nr3

---
+++ Zadania3/zad4.py
@@ -0,0 +1,14 @@
+
+def prostokatny(a,b,c):
+    if a**2+b**2==c**2:
+        print("To jest trójkąt prostokątny")
+    else:
+        print("To nie jest trójkąt prostokątny")
+
+#a i b to krótsze boki, c to najdłuższy bok
+
+prostokatny(3,4,5)
+
+
+
+
---
+++ Zadania3/zad5.py
@@ -0,0 +1,4 @@
+def ptrapez(a= 3,b= 4,h= 5):
+    return (((a+b)*h)/2)
+
+print(ptrapez())
