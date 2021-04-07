b9d179c9a4d01ca9cdc340c83318eceb4ea559fc
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-04-07 19:00:13 +0200

    Update zad4.py

--- Zadania4/zad4.py
+++ Zadania4/zad4.py
@@ -2,4 +2,4 @@ from PlikiDodatk import NaZakupy
 produkty = NaZakupy.NaZakupy("Truskawki", 3, "Kg", 2)
 produkty.wyswietl_Produkt()
 produkty.ile_produktu()
-print(produkty.ile_kosztuje())
+produkty.ile_kosztuje()
